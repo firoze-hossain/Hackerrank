2de1e5992dda955b23269d450ef25ee1f7bea403
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-11-09 14:38:15 +0600

    Simple Array Sum

---
+++ src/com.hackerrank.learn/algorithms/warmup/simpleArraySum/SimpleArraySum.java
@@ -0,0 +1,43 @@
+package algorithms.warmup.simpleArraySum;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SimpleArraySum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(
+                new FileWriter("C:\\Users\\firoze.hossain\\Documents\\path\\file.txt"));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int result = Result.simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+class Result {
+    public static int simpleArraySum(List<Integer> ar) {
+        int sum = 0;
+        for (int i = 0; i < ar.size(); i++) {
+            sum += ar.get(i);
+        }
+        return sum;
+    }
+
+}
