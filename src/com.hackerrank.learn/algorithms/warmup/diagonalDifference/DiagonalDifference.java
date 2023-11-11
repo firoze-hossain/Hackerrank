827256ae0c2b5ada34fbf3a74c6cd08fc71bb191
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-11-11 19:13:56 +0600

    Diagonal Difference

---
+++ src/com.hackerrank.learn/algorithms/warmup/diagonalDifference/DiagonalDifference.java
@@ -0,0 +1,64 @@
+package algorithms.warmup.diagonalDifference;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class DiagonalDifference {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(
+                new FileWriter("/home/firoze_hossain/Documents/Hello/hello.txt"));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(Collectors.toList()));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+class Result {
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        // Write your code here
+        int rightDiagonal = 0;
+        int leftDiagonal = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = 0; j < arr.size(); j++) {
+                if (i == j) {
+                    rightDiagonal += arr.get(i).get(j);
+                }
+                if (i == arr.size() - j - 1) {
+                    leftDiagonal += arr.get(i).get(j);
+                }
+            }
+        }
+        return Math.abs(rightDiagonal - leftDiagonal);
+    }
+
+}
