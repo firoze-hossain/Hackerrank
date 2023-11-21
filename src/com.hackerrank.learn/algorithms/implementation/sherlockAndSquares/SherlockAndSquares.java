9925f124244bd4d547eebe97d6188cb4cdae72de
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-11-21 13:04:02 +0600

    Sherlock and Squares

---
+++ src/com.hackerrank.learn/algorithms/implementation/sherlockAndSquares/SherlockAndSquares.java
@@ -0,0 +1,44 @@
+package algorithms.implementation.sherlockAndSquares;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+public class SherlockAndSquares {
+    public static int squares(int a, int b) {
+        int ceilA = (int) Math.ceil(Math.sqrt(a));
+        int floorB = (int) Math.floor(Math.sqrt(b));
+        return floorB - ceilA + 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(
+                new FileWriter("C:\\Users\\firoze.hossain\\Documents\\path\\file.txt"));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int a = Integer.parseInt(firstMultipleInput[0]);
+
+                int b = Integer.parseInt(firstMultipleInput[1]);
+
+                int result = squares(a, b);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
