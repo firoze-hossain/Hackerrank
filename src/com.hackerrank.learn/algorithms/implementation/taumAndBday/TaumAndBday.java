4fd00293caad01e06ec83ae484ef437c4dbb4c48
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-12-05 23:12:24 +0600

    Taum and B'day

---
+++ src/com.hackerrank.learn/algorithms/implementation/taumAndBday/TaumAndBday.java
@@ -0,0 +1,52 @@
+package algorithms.implementation.taumAndBday;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+public class TaumAndBday {
+    public static long taumBday(int b, int w, int bc, int wc, int z) {
+        long minBlackPrice = Math.min(bc, wc + z);
+        long minWhitePrice = Math.min(wc, bc + z);
+        long result = (minBlackPrice * b) + (minWhitePrice * w);
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/home/firoze/Documents/file.txt"));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int b = Integer.parseInt(firstMultipleInput[0]);
+
+                int w = Integer.parseInt(firstMultipleInput[1]);
+
+                String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int bc = Integer.parseInt(secondMultipleInput[0]);
+
+                int wc = Integer.parseInt(secondMultipleInput[1]);
+
+                int z = Integer.parseInt(secondMultipleInput[2]);
+
+                long result = taumBday(b, w, bc, wc, z);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
